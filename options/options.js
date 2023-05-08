1d24210cd7d665ea9600b6c0e43914e6fdf20ae4
Crowdbotics <engineering@crowdbotics.com>
2023-05-08 10:43:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-meadow-de-82068",
+  url: "https://weathered-meadow-de-82068.botics.co",
+  api: "https://weathered-meadow-de-82068.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
